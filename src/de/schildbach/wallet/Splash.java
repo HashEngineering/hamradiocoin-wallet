61099b154e427586d94a440bb7203961674739b3
Srecko Lasbaher <sreellee@gmail.com>
2015-02-09 02:59:06 +0100

    Android-wallet v0.0.1 not working
    This version doesn’t work, but it will soon.

---
+++ src/de/schildbach/wallet/Splash.java
@@ -0,0 +1,20 @@
+package de.schildbach.wallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import hashengineering.digitalcoin.wallet.R;
+
+/**
+ * Created by sreckolasbaher on 2/9/15.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+    }
+}
